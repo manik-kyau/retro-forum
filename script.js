78fad27c35657e29be962b053438de51b589c842
manik-kyau <you@example.commaniksarkar.kyau.cse>
2024-03-04 09:58:04 -0800

    all project done

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const loadAllData = async(inputText) =>{
     const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${inputText}`);
     const data = await response.json();
     const allPost = data.posts;
-    // console.log(allPost)
-    displayAllData(allPost);
+    setTimeout(() => {
+        displayAllData(allPost);
+    },2000);
 }
+
 const displayAllData = (allPost) =>{
-    // console.log(allPost);
+    const errorElement =document.getElementById('error-element');
+
     const postContainer = document.getElementById('post-container');
     postContainer.innerHTML = '';
+    if(allPost.length === 0){
+        errorElement.classList.remove('hidden')
+      }
+      else{
+        errorElement.classList.add('hidden')
+      }
     allPost.forEach((post) =>{
-        // console.log(post);
-        let activateBadge ='';
+            let activateBadge ='';
         if(post.isActive){
             activateBadge = `<span class="indicator-item badge bg-green-600"></span>`;
         }
@@ -20,7 +28,7 @@ const displayAllData = (allPost) =>{
         }
         const postCard = document.createElement('div');
         postCard.innerHTML = `
-        <div class="flex flex-col lg:flex-row gap-4 lg:gap-6 border p-5 lg:p-8 rounded-3xl">
+        <div class="flex flex-col lg:flex-row gap-4 lg:gap-6 border p-5 bg-slate-50 lg:p-8 rounded-3xl">
         <div>
             <div class="indicator">
                 ${activateBadge} 
@@ -30,29 +38,29 @@ const displayAllData = (allPost) =>{
         <div class="w-full">
             <!-- child-1-->
             <div class="flex gap-5">
-                <h4 class="text-sm font-medium text-[#12132dcc]">#${post.category}</h4>
-                <h4 class="text-sm font-medium text-[#12132dcc]">Author: ${post.author.name}</h4>
+                <h4 class="text-sm font-medium text-[#12132dcc] font-inter">#${post.category}</h4>
+                <h4 class="text-sm font-medium text-[#12132dcc] font-inter">Author: ${post.author.name}</h4>
             </div>
             <!-- child-2-->
             <div class="mt-2 lg:mt-3">
             <!--  post Title   -->
-                <h2 id="post-title" class="text-lg lg:text-xl font-bold text-[#12132D]">${post.title}</h2>
-                <p class="text-sm lg:text-base font-normal text-[#12132d99] border-dashed border-b-2 py-3 lg:py-4">${post.description}</p>
+                <h2 id="post-title" class="text-lg lg:text-xl font-bold text-[#12132D] font-mulish">${post.title}</h2>
+                <p class="text-sm lg:text-base font-normal text-[#12132d99] border-dashed border-b-2 py-3 lg:py-4 font-inter">${post.description}</p>
             </div>
             <!-- child-3-->
             <div class="flex justify-between mt-3 lg:mt-5">
                 <div class="flex gap-5 lg:gap-8">
                     <div class="flex items-center gap-2">
                         <img class="h-5 w-5" src="icon/sms.png" alt="">
-                        <p class="text-base font-normal text-[#12132d99]">${post.comment_count}</p>
+                        <p class="text-base font-normal text-[#12132d99] font-inter">${post.comment_count}</p>
                     </div>
                     <div id="view-count" class="flex items-center gap-1">
                         <img class="h-5 w-6" src="icon/eye.png" alt="">
-                        <p class="view text-base font-normal text-[#12132d99]">${post.view_count}</p>
+                        <p class="view text-base font-normal text-[#12132d99] font-inter">${post.view_count}</p>
                     </div>
                     <div class="flex items-center gap-1">
                         <img class="h-5 w-5" src="icon/view.png" alt="">
-                        <p class="text-base font-normal text-[#12132d99]">${post.posted_time} min</p>
+                        <p class="text-base font-normal text-[#12132d99] font-inter">${post.posted_time} min</p>
                     </div>
                 </div>
                 <div>
@@ -67,13 +75,11 @@ const displayAllData = (allPost) =>{
         `;
         postContainer.appendChild(postCard);
     })
-    toggleLoadingSpinner(false);
 
     const postButton = document.getElementsByClassName('post-button');
     let count = 0;
     for(let i = 0; i< postButton.length; i++){
-        const bttn = postButton[i]
-        // console.log(bttn);
+        const bttn = postButton[i];
         bttn.addEventListener('click',function(event){
             count++;
             document.getElementById('card-count').innerText = count;
@@ -95,13 +101,16 @@ const displayAllData = (allPost) =>{
             titleContainer.appendChild(div);
         })
     }
+    toggleLoadingSpinner(false);
 }
+loadAllData('comedy');
 
 // Handle search Option
 const handleSearch = () =>{
     toggleLoadingSpinner(true)
     const inputField = document.getElementById('input-field');
     const inputText = inputField.value;
+    inputField.value='';
     loadAllData(inputText);
 };
 
@@ -115,9 +124,6 @@ const toggleLoadingSpinner = (isLoading) =>{
         loadingSpinner.classList.add('hidden');
     }
 }
-
-loadAllData('comedy');
-
 
 // Work of letest Posts section
 const loadLetestPosts = async() =>{
@@ -139,21 +145,21 @@ const displayLetestPosts = (letestPosts) =>{
         <div class="mt-5 space-y-4">
             <div class="flex gap-2">
                 <img src="icon/date.png" alt="">
-                <h4 class="text-base font-normal text-[#12132d99]">${postedDate}</h4>
+                <h4 class="text-base font-normal text-[#12132d99] font-mulish">${postedDate}</h4>
             </div>
-          <h2 class="text-lg font-extrabold text-[#12132D]">${ltstPost.title}</h2>
-          <p class="text-base font-normal text-[#12132d99]">${ltstPost.description.slice(0,85)}</p>
+          <h2 class="text-lg font-extrabold text-[#12132D] font-mulish">${ltstPost.title}</h2>
+          <p class="text-base font-normal text-[#12132d99] font-mulish">${ltstPost.description.slice(0,85)}</p>
           <div class="card-actions">
             <img class="h-12 w-12 rounded-full" src="${ltstPost.profile_image}" alt="">
             <div>
-                <h4 class="text-base font-bold text-[#12132D]">${ltstPost?.author?.name}</h4>
-                <p class="text-sm font-normal text-[#12132d99]">${desigNation}</p>
+                <h4 class="text-base font-bold text-[#12132D] font-mulish">${ltstPost?.author?.name}</h4>
+                <p class="text-sm font-normal text-[#12132d99] font-mulish">${desigNation}</p>
             </div>
           </div>
         </div>
         </div>
         `;
-        letestPostContainer.appendChild(letestPostCard)
+        letestPostContainer.appendChild(letestPostCard);
     })
 }
 loadLetestPosts();
